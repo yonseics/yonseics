050884bfec201e9c79ac0a7cb86c9cb2678b8481
Seung.gi.Lim <seung.gi.lim@gmail.com>
2014-01-19 00:10:45 -0500

    Fixed board admin permission.
    # Fixed querysets of bulletin.

--- combaragi/ccboard/admin.py
+++ combaragi/ccboard/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models.query_utils import Q
 from combaragi.ccboard.models import Board, Bulletin, RelatedPosition, RelatedFile, PhotoTag, Scrap, Category
 
 admin.site.register(Board)
@@ -18,5 +19,16 @@ class MyBulletinAdmin(admin.ModelAdmin):
             self.exclude = ('parent',)
         field_list = super(MyBulletinAdmin, self).get_fieldsets(request, obj)
         return field_list
+
+    def queryset(self, request):
+        qs = super(MyBulletinAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(
+            Q(isHiddenUser=False) &
+            Q(deleted=False) &
+            Q(board__secret=False)
+        )
+
 
 admin.site.register(Bulletin, MyBulletinAdmin)
